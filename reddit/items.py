668d91b03b342df3e97695cb41babcacd5bc8e41
ruhomor <ruhomor@icloud.com>
2020-08-19 22:51:52 +0300

    no scroll crawl

--- reddit/items.py
+++ reddit/items.py
@@ -8,22 +8,8 @@ from scrapy import Item, Field
 
 class RedditPost(Item):
     link = Field()
-    author = Field()
+    author = Field() # doesn't load for some unknown reason
     date = Field()
-    text = Field()
-    img_file = Field()
-    vote_count = Field()
-    award_count = Field()
-    comment_count = Field()
-    caption = Field()
-    origin = Field()
-    pass
-
-class SubReddit(Item):
-    name = Field()
-    link = Field()
-    about = Field()
-    rules = Field()
-    similar = Field()
-    moderators = Field()
+    title = Field()
+    img = Field()
     pass
--- reddit/spiders/reddit-spider.py
+++ reddit/spiders/reddit-spider.py
@@ -1,5 +1,5 @@
 from scrapy import Spider, Request
-from reddit.items import RedditItem
+from reddit.items import RedditPost
 from reddit import pipelines
 
 
@@ -15,15 +15,19 @@ class RedditSpider(Spider):
         for subreddit in subreddits:
             url = f"https://www.reddit.com/r/{subreddit}/"
             request = Request(url, callback=self.parse_subreddit)
-            request.meta["subreddit"] = subreddit
             yield request
 
     def parse_subreddit(self, response):
         meme = RedditPost()
 
         post_wrapper = response.xpath("//div[@class = 'rpBJOHq2PR60pnwJlUyP0']")
-        for post in post_wrapper.xpath("/div")
-        meme["origin"] = response.meta["subreddit"]
-        meme["author"] = post.xpath["//*[@id='t3_ic9uxz']/div[2]/div[1]/div/div[1]/div/a"]
+        for post in post_wrapper.xpath("./div"):
+            print("woah post") # debug
+            meme["title"] = post.xpath(".//h3[@class='_eYtD2XCVieq6emjKBH3m']/text()").extract()
+            meme["author"] = None # doesn't load for some unknown reason
+            meme["date"] = post.xpath(".//a[@class='_3jOxDPIQ0KaOWpzvSQo-1s']/text()").extract()
+            meme["link"] = post.xpath(".//a[@data-click-id='comments']/@href").extract()
+
+            yield meme
 
 
