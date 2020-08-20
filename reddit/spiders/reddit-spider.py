b0290de0c943ab93572a3146bf358017223a290d
ruhomor <ruhomor@icloud.com>
2020-08-20 08:52:11 +0300

    nothings scraping fsgdr

--- reddit/spiders/reddit-spider.py
+++ reddit/spiders/reddit-spider.py
@@ -1,48 +1,98 @@
 from scrapy import Spider, Request, FormRequest
 from reddit.items import RedditPost
+from urllib.parse import urlencode
+import json
 from reddit import pipelines
 
 
 class RedditSpider(Spider):
     name = "reddit"
+    i = 0
     allowed_domains = ["reddit.com"]
     start_urls = ["https://www.reddit.com/login/"]
+    subreddits = ["meme",
+                  "dankmemes",
+                  "PoliticalHumor",
+                  "Anarcho_Capitalism",
+                  "ProgrammerAnimemes"]
+    scroll_urls = {e: f"https://gateway.reddit.com/desktopapi/v1/subreddits/{e}?" for e in subreddits}
+    params = {e: {"rtj": "only",
+              "redditWebClient": "web2x",
+              "app": "web2x-client-production",
+              "allow_over18": "1",  # adult content lol
+              "include": "prefsSubreddit",
+              "after": "t3_icgvnd",
+              "dist": "1",
+              "layout": "card",
+              "sort": "hot",
+              "geo_filter": "RU"
+              } for e in subreddits}
 
-    def parse(self, response): # login function
+
+    def parse(self, response):  # login function
         csrf_token = response.xpath("//input[@name='csrf_token']/@value").extract_first()
-        print("TOKEN------------")
+        print("TOKEN------------LOGIN")
         print(csrf_token)
-        print("TOKEN------------")
+        print("TOKEN------------LOGIN")
         yield FormRequest.from_response(response,
-                                         formdata={"csrf_token": csrf_token,
-                                                   "username": "belkbe1ka",
-                                                   "password": "be1kabe1ka",
-                                                   "dest": "https://www.reddit.com/"},
-                                         callback=self.after_login)
+                                        formdata={"csrf_token": csrf_token,
+                                                  "username": "belkbe1ka",
+                                                  "password": "be1kabe1ka",
+                                                  "dest": "https://www.reddit.com/"},
+                                        callback=self.after_login)
 
-    def after_login(self, response): # creates request for each subreddit to crawl
+#    def after_login(self, response): # creates request for each subreddit to crawl
+#
+ #       for subreddit in subreddits:
+  #          self.subreddit = subreddit
+   #         url = f"https://www.reddit.com/r/{subreddit}/"
+    #        request = Request(url, callback=self.parse_subreddit)
+     #       yield request
 
-        subreddits = ["meme",
-                      "dankmemes",
-                      "PoliticalHumor",
-                      "Anarcho_Capitalism",
-                      "ProgrammerAnimemes"]
-        for subreddit in subreddits:
-            url = f"https://www.reddit.com/r/{subreddit}/"
-            request = Request(url, callback=self.parse_subreddit)
-            yield request
+    def after_login(self, response):  # creates request for each subreddit to crawl
+        for subreddit in self.subreddits:
+            print("----------{}---------".format(subreddit))  # debug
+            url = self.scroll_urls[subreddit] + urlencode(self.params[subreddit])
+            yield Request(url, callback=self.parse_subreddit_scroll)
+
+    def parse_subreddit_scroll(self, response):
+        print("---------PARSING_PAGE----------")  # debug
+        meme = RedditPost()
+
+        json_data = json.loads(response.text)
+        subreddit = json_data["subreddits"][str(list(json_data["subreddits"].keys())[0])]["name"]  # Stupid as f
+        print(subreddit)
+        print(subreddit)
+        print(subreddit)
+        print(subreddit)
+        print(subreddit)
+
+        post_wrapper = response.xpath("//div[@class = 'rpBJOHq2PR60pnwJlUyP0']")
+        for post in post_wrapper.xpath("./div"):  # TODO skip parsed posts
+            print("----------!Woah Post!----------")  # debug
+            meme["title"] = post.xpath(".//h3[@class='_eYtD2XCVieq6emjKBH3m']/text()").extract()
+            meme["author"] = None  # doesn't load for some unknown reason
+            meme["date"] = post.xpath(".//a[@class='_3jOxDPIQ0KaOWpzvSQo-1s']/text()").extract()
+            meme["link"] = post.xpath(".//a[@data-click-id='comments']/@href").extract()
+            yield meme
+
+        self.params[subreddit]["after"] = json_data["token"]
+        self.params[subreddit]["dist"] = json_data["dist"]
+
+        for i in range(0, 3):
+            url = self.scroll_urls[subreddit] + urlencode(self.params[subreddit])
+            print("-----------SCROLLING-----------")  # debug
+            yield Request(url, callback=self.parse_subreddit)
 
     def parse_subreddit(self, response):
+        print("---------PARSING_PAGE----------")  # debug
         meme = RedditPost()
 
         post_wrapper = response.xpath("//div[@class = 'rpBJOHq2PR60pnwJlUyP0']")
-        for post in post_wrapper.xpath("./div"):
-            print("woah post") # debug
+        for post in post_wrapper.xpath("./div"):  # TODO skip parsed posts
+            print("----------!Woah Post!----------")  # debug
             meme["title"] = post.xpath(".//h3[@class='_eYtD2XCVieq6emjKBH3m']/text()").extract()
-            meme["author"] = None # doesn't load for some unknown reason
+            meme["author"] = None  # doesn't load for some unknown reason
             meme["date"] = post.xpath(".//a[@class='_3jOxDPIQ0KaOWpzvSQo-1s']/text()").extract()
             meme["link"] = post.xpath(".//a[@data-click-id='comments']/@href").extract()
-
             yield meme
-
-
